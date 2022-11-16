bb0eeb35a5110b60d48f0578e1c549136c0926da
salsabeelnoor <salsabeelnoor@gmail.com>
2022-11-16 19:11:52 +0600

    bonus

--- index.js
+++ index.js
@@ -53,8 +53,30 @@ async function run() {
 
     //get all services
     app.get("/services", async (req, res) => {
-      const query = {}; //to find everything
-      const cursor = serviceCollection.find(query);
+      const search = req.query.search;
+      console.log(search);
+      // const query = {
+      //   $text: {
+      //     $search: search,
+      //   },
+      // };
+      // const query = { price: { $gt: 100, $lt: 300 } }; //to find less than 100
+      //to find grater than use $gt, for equal use eq
+      //grater than or equal => gte
+      //less than or equal => lte
+      //less than or equal => leq
+      //not than or equal => ne
+      //in, not in => in, nin
+      let query = {};
+      if (search.length) {
+        query = {
+          $text: {
+            $search: search,
+          },
+        };
+      }
+      const order = req.query.order === "asc" ? 1 : -1;
+      const cursor = serviceCollection.find(query).sort({ price: order });
       const services = await cursor.toArray();
       res.send(services);
     });
